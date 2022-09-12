cb48f319fe2324b82a846ba89b5183fdaf5a03ba
likithkj <85862218+likithkj@users.noreply.github.com>
2022-09-12 18:06:38 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+const {createClient} = require('@supabase/supabase-js')
+
+const supabase = createClient('https://rqotjimiqgpllnwxxwek.supabase.co','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJxb3RqaW1pcWdwbGxud3h4d2VrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjI5ODM1MzQsImV4cCI6MTk3ODU1OTUzNH0.hzPTx166pYy-9KvgQIwxFOnhJ5zndOWcpsc5PbFvzl0)
+
+console.log(supabase);
